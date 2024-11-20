b249ba9e26579fc997dfd9012b1671caa542bccc
Noor El-Din Walid <noorwalid72@yahoo.com>
2024-11-20 19:00:28 +0200

    Initial Commit

---
+++ SwiftUI-State-management/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftUI-State-management
+//
+//  Created by Noor El-Din Walid on 20/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
